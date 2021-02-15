92ebb31b9d5a1418b9ab82e2b52c3e608ce68c10
Niv R <nivr@easy.co.il>
2021-02-15 21:41:46 +0200

    Updates

---
+++ precache-manifest.84277bcd909eaaf4027da1b2249d5ca6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f63badb395775aecc04cc8580492fc",
+    "url": "/geo_project/index.html"
+  },
+  {
+    "revision": "43dd57eec1537033815c",
+    "url": "/geo_project/static/css/2.69ac34f2.chunk.css"
+  },
+  {
+    "revision": "71ed44dac5c5743b85de",
+    "url": "/geo_project/static/css/main.a39066ea.chunk.css"
+  },
+  {
+    "revision": "43dd57eec1537033815c",
+    "url": "/geo_project/static/js/2.ca0f1bc2.chunk.js"
+  },
+  {
+    "revision": "cdef75879235130052acc2626c717c44",
+    "url": "/geo_project/static/js/2.ca0f1bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ed44dac5c5743b85de",
+    "url": "/geo_project/static/js/main.87fc6f3d.chunk.js"
+  },
+  {
+    "revision": "740182894a80cc445d8e",
+    "url": "/geo_project/static/js/runtime-main.544fcb33.js"
+  },
+  {
+    "revision": "2ceaf661f7c099123415f94f97e2a0c5",
+    "url": "/geo_project/static/media/venue_location_icon.2ceaf661.svg"
+  }
+]);
