380f45c1e6dff8878b064d20e9957585f3b6ffd2
Niv R <nivr@easy.co.il>
2021-02-21 22:13:12 +0200

    comments fixed

--- src/components/VenueMarkers.js
+++ src/components/VenueMarkers.js
@@ -6,9 +6,9 @@ import MarkerPopup from "./MarkerPopup";
 const VenueMarkers = (props) => {
   const { marks,showGroupStatus,updateDB } = props;
   console.log('in vnenue: '   +JSON.stringify(showGroupStatus))
-  const city_sub_sets = Object.keys(marks)
+  const city_sub_sets = marks? Object.keys(marks) :undefined
   //const keys = Object.keys(showGroupStatus)
-  const city_marks = city_sub_sets.reduce( (acc,sub_set_name) => showGroupStatus[sub_set_name]? acc.concat(
+  const city_marks = city_sub_sets? city_sub_sets.reduce( (acc,sub_set_name) => showGroupStatus[sub_set_name]? acc.concat(
     marks[sub_set_name].map((venue, i) => (
       <Marker key={i} position={venue.geometry} icon={VenueLocationIcon}>
         <MarkerPopup updateDB={updateDB} data={venue} />
@@ -17,7 +17,7 @@ const VenueMarkers = (props) => {
   )) 
   : acc
   
-   ,[]);
+   ,[]) :undefined
 
 
 
